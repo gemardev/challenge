4324f090baa4d1c46c5c612b19cc0627d843089d
erzed <mail.rizkytegar@gmail.com>
2022-12-20 13:18:54 +0700

    Add files via upload

---
+++ coding_challenge/HelloKitty.dart
@@ -0,0 +1,34 @@
+/* 
+===================================
+=      [ GK ]  CHALLENGE ROOM     =
+=  Author   : @rizkytegar         =
+=  Team     : Gemar Kode          =
+=  Category : Coding Challenge    =
+===================================
+*/
+
+void main() {
+
+  String  ox12e = 'SfGSGB4W7f{gvfRGAmM9d170JMg8}gMjM8G{n', 
+          ox13e = 'ORRAMdhF{MoxGWa52QFfvG{DtoCkd{OjjajDne4iJr', 
+          ox14e = '1RM2Macy3m}Rz2opMle6upPhYVTbz}dLEEM9o{tnM}aTvSo8Xt';
+
+  int ox1e = 0, 
+      ox2e = 1, 
+      ox3e; 
+
+  String ox0m = '? + ? + ?';
+    
+  for (...) { 
+
+    if(...){
+
+      print(... + index );
+   
+    }
+
+  }
+
+}
+
+
